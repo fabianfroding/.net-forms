e287ee4969c447e5739fd91ba53b015b37de8b9c
fabianfroding <fabian.froding@gmail.com>
2020-03-13 12:00:31 +0100

    Worked on documentation and class diagram. Started worked on UI

--- MediaShop/MainForm.Designer.cs
+++ MediaShop/MainForm.Designer.cs
@@ -28,21 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.BTNExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BTNExit
+            // 
+            this.BTNExit.BackColor = System.Drawing.Color.Firebrick;
+            this.BTNExit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BTNExit.FlatAppearance.BorderSize = 0;
+            this.BTNExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTNExit.ForeColor = System.Drawing.Color.White;
+            this.BTNExit.Location = new System.Drawing.Point(1045, 12);
+            this.BTNExit.Name = "BTNExit";
+            this.BTNExit.Size = new System.Drawing.Size(32, 32);
+            this.BTNExit.TabIndex = 0;
+            this.BTNExit.Text = "X";
+            this.BTNExit.UseVisualStyleBackColor = false;
+            this.BTNExit.Click += new System.EventHandler(this.BTNExit_Click);
             // 
             // UI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
-            this.ClientSize = new System.Drawing.Size(1140, 732);
+            this.ClientSize = new System.Drawing.Size(1089, 627);
+            this.Controls.Add(this.BTNExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UI";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button BTNExit;
     }
 }
 
--- MediaShop/MainForm.cs
+++ MediaShop/MainForm.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace MediaShop
 {
     public partial class UI : Form
     {
+        private ProductController productController;
         public UI()
         {
             InitializeComponent();
+            productController = new ProductController();
+        }
+
+        private void BTNExit_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
