bb350a91c38253bb523918d80a8d46992d4fa581
fabianfroding <fabian.froding@gmail.com>
2020-03-18 14:54:49 +0100

    Finished media shop class diagram. Worked on documentation for media shop. Updated properties of product model class

--- MediaShop/Models/Product.cs
+++ MediaShop/Models/Product.cs
@@ -7,6 +7,8 @@
         public string name { get; set; }
         public double price { get; set; }
         public int stock { get; set; }
+        public int unitsSold { get; set; }
+        public string dateSold { get; set; }
         public ProductType productType { get; set; }
 
         public Product()
