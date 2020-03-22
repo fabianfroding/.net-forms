4571b2bdbe272fcb782ef2c804b646504deeb541
fabianfroding <fabian.froding@gmail.com>
2020-03-22 17:10:11 +0100

    Started working on login form

---
+++ MediaShop/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MediaShop
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MediaShop/MainForm.Designer.cs
+++ MediaShop/MainForm.Designer.cs
@@ -32,6 +32,7 @@
             this.BTNExit = new System.Windows.Forms.Button();
             this.BTNNewProduct = new System.Windows.Forms.Button();
             this.ListBoxProducts = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BTNExit
@@ -69,11 +70,21 @@
             this.ListBoxProducts.TabIndex = 3;
             this.ListBoxProducts.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListBoxProducts_MouseDoubleClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "←Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
-            this.ClientSize = new System.Drawing.Size(347, 414);
+            this.ClientSize = new System.Drawing.Size(330, 439);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ListBoxProducts);
             this.Controls.Add(this.BTNNewProduct);
             this.Controls.Add(this.BTNExit);
@@ -91,6 +102,7 @@
         private System.Windows.Forms.Button BTNExit;
         private System.Windows.Forms.Button BTNNewProduct;
         private System.Windows.Forms.ListBox ListBoxProducts;
+        private System.Windows.Forms.Button button1;
     }
 }
 
